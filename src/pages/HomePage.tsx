e99285a67cc6ec58e5d6922543c267db942b2ba3
Sahil Khan <skhantis05@gmail.com>
2025-04-24 10:06:48 +0000

    commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -104,24 +104,7 @@ const MouseFollower = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [isVisible, setIsVisible] = useState(false);
   
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-      if (!isVisible) setIsVisible(true);
-    };
-    
-    const handleMouseLeave = () => {
-      setIsVisible(false);
-    };
-    
-    window.addEventListener("mousemove", handleMouseMove);
-    document.body.addEventListener("mouseleave", handleMouseLeave);
-    
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-      document.body.removeEventListener("mouseleave", handleMouseLeave);
-    };
-  }, [isVisible]);
+
   
   return (
     <AnimatePresence>
